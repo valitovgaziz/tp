bd05ac48dfba2d61340173d0c6c6facbb39f1dec
valitovgaziz <a-c-v-a-l@mail.ru>
2024-08-07 22:50:50 +0500

    goos added. Configs need

--- migrations/20240806055315_init_user_table.sql
+++ migrations/20240806055315_init_user_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 -- +goose StatementBegin
 CREATE TABLE IF NOT EXISTS users(
-    id UUID PRIMARY KEY NOT NULL,
+    Id UUID PRIMARY KEY NOT NULL,
     name VARCHAR(50) NOT NULL,
     email VARCHAR(50),
     password VARCHAR(1000) NOT NULL,
