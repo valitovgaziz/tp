45a13df2b9047060539f564d962dc1e5947f1c3f
valitovgaziz <a-c-v-a-l@mail.ru>
2024-08-13 16:33:46 +0500

    migration create extension uuid-ossp

--- api/src/models/user.go
+++ api/src/models/user.go
@@ -3,7 +3,7 @@ package models
 import "github.com/google/uuid"
 
 type User struct {
-	Id       uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4;primaryKey;unique;AutoIncrement:false"`
+	Id       uuid.UUID `json:"id" gorm:"type:uuid;default:uuid_generate_v4();primaryKey;unique;AutoIncrement:false"`
 	Name     string    `json:"name" gorm:"type:string"`
 	Email    string    `json:"email" gorm:"type:string;index"`
 	Password string    `json:"password" gorm:"type:string;index"`
