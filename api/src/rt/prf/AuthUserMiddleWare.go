3b045c8629b7210c45de748736c11cccd68afa70
valitovgaziz <a-c-v-a-l@mail.ru>
2024-08-22 07:40:15 +0500

    add files for Authorization middlewaries Admin and User

---
+++ api/src/rt/prf/AuthUserMiddleWare.go
@@ -0,0 +1,9 @@
+package prf
+
+import "net/http"
+
+func AuthUserMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusUnauthorized)
+	})
+}
