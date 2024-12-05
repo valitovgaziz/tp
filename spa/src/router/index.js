47a9e724abd5f6a361815e7bfbe9f70b6ef7a620
valitovgaziz <a-c-v-a-l@mail.ru>
2024-12-05 22:49:33 +0500

    SignUP.vue component added

--- spa/src/router/index.js
+++ spa/src/router/index.js
@@ -8,6 +8,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SingIn,
     }
   ],
 })
