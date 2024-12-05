b87c3aa1ae30408b93d0fea8027ef70072c9b4d7
valitovgaziz <a-c-v-a-l@mail.ru>
2024-12-06 01:55:00 +0500

    need fix rout spa and set api on URL

--- spa/src/router/index.js
+++ spa/src/router/index.js
@@ -1,20 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignUpView from '../views/SingUpV.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView,
+    },
+    {
       path: '/',
       name: 'home',
       component: HomeView,
     },
-    {
-      path: '/signin',
-      name: 'signin',
-      component: SingIn,
-    }
   ],
-})
+});
 
 export default router
---
+++ spa/src/views/SingUpV.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
+import SignUpC from '../components/SignUpC.vue'
+</script>
+
+<template>
+  <Header />
+  <SignUpC />
+  <Footer />
+</template>
